9c1a7824e24d4a1523ece1278080bf8a5a00bba7
Artem Mukhamatyanov <ausupertramp@gmail.com>
2017-10-10 22:36:23 +0300

    added modal

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Nav, Navbar, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router';
-import { LinkContainer } from 'react-router-bootstrap';
+import { LinkContainer, IndexLinkContainer } from 'react-router-bootstrap';
 
 const Navigation = () => {
 	return(
@@ -10,20 +10,30 @@ const Navigation = () => {
 				<Navbar.Brand>InvoiceApp</Navbar.Brand>
 			</Navbar.Header>
 			<Nav>
-				{/*<LinkContainer to='/'>*/}
-					{/*<NavItem eventKey={1}>*/}
-						{/*Invoices*/}
-					{/*</NavItem>*/}
-				{/*</LinkContainer>*/}
-				<NavItem eventKey={1}>
-					<Link to='/'>Invoices</Link>
-				</NavItem>
-				<NavItem eventKey={2}>
-					<Link to='/products'>Products</Link>
-				</NavItem>
-				<NavItem eventKey={3}>
-					<Link to='/customers'>Customers</Link>
-				</NavItem>
+				<LinkContainer to='/invoices'>
+					<NavItem eventKey={1}>
+						Invoices
+					</NavItem>
+				</LinkContainer>
+				<LinkContainer to='/products'>
+					<NavItem eventKey={2}>
+						Products
+					</NavItem>
+				</LinkContainer>
+				<LinkContainer to='/customers'>
+					<NavItem eventKey={3}>
+						Customers
+					</NavItem>
+				</LinkContainer>
+				{/*<NavItem eventKey={1}>*/}
+					{/*<Link to='/'>Invoices</Link>*/}
+				{/*</NavItem>*/}
+				{/*<NavItem eventKey={2}>*/}
+					{/*<Link to='/products'>Products</Link>*/}
+				{/*</NavItem>*/}
+				{/*<NavItem eventKey={3}>*/}
+					{/*<Link to='/customers'>Customers</Link>*/}
+				{/*</NavItem>*/}
 			</Nav>
 		</Navbar>
 	)
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import Customers from './components/customers';
-import Products from './components/products'
+import Products from './components/products';
+import Invoices from './components/invoices';
 
 export default (
 	<Route path='/' component={App}>
-		<Route path='customers' component={Customers} />
+		<Route path='invoices' component={Invoices} />
 		<Route path='products' component={Products} />
+		<Route path='customers' component={Customers} />
 	</Route>
 )
